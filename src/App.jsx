d3a11de89c5f7dce0176c1e8fc29346a11b2bdf0
Lincikas <118252289+Lincikas@users.noreply.github.com>
2023-03-03 15:55:44 +0200

    comit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch('https://v2.jokeapi.dev/joke/Programming?amount=10')
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+    }, []);
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <>
+                <h1>Jokes-on-you</h1>
+                <div>
+                    {items.jokes.map((item) => {
+                        if (item.type === 'single') {
+                            return (
+                                <div key={item.id} className='card'>
+                                    <p>{item.joke}</p>
+                                </div>
+                            );
+                        } else {
+                            return (
+                                <div key={item.id} className='card'>
+                                    <p> Setup: {item.setup}</p>
+                                    <p> Delivery: {item.delivery}</p>
+                                </div>
+                            );
+                        }
+                    })}
+                </div>
+            </>
+        );
+    }
+}
+
+export default App;
